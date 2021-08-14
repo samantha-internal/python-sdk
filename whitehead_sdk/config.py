a24f47134b1456cffe04e53bf6dcf376e9c56c91
whiterabbit1983 <asmatic075@gmail.com>
2021-08-14 13:02:36 +0300

    fix: Update endpoint URLs

--- whitehead_sdk/config.py
+++ whitehead_sdk/config.py
@@ -1,5 +1,5 @@
 import os
 
 
-API_ENDPOINT = os.environ.get("WHITEHEAD_API_ENDPOINT", "https://api.mauna.cloud/v1/graphql")
-AUTH_ENDPOINT = os.environ.get("WHITEHEAD_AUTH_ENDPOINT", "https://sdk.mauna.cloud/generateHasuraJWT")
+API_ENDPOINT = os.environ.get("WHITEHEAD_API_ENDPOINT", "https://api.whitehead.ai/hasura/v1/graphql")
+AUTH_ENDPOINT = os.environ.get("WHITEHEAD_AUTH_ENDPOINT", "https://auth.whitehead.ai/generateHasuraJWT")
